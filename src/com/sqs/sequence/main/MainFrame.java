168158c6d7e64bd2066591538810a72b136d987c
zhangdong <zhangdong@zhangdongdeMacBook-Air.local>
2016-11-02 22:51:55 +0800

    添加从文件中读取的功能

--- src/com/sqs/sequence/main/MainFrame.java
+++ src/com/sqs/sequence/main/MainFrame.java
@@ -11,8 +11,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 
 import javax.swing.BorderFactory;
@@ -144,7 +146,29 @@ public class MainFrame extends JFrame{
 					e2.printStackTrace();
 				}
 			} else if (e.getActionCommand().equals("Open")) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setDialogTitle("Choose a file");
+				int result = fileChooser.showOpenDialog(MainFrame.this);
+				if (result != JFileChooser.APPROVE_OPTION) {
+					return;
+				}
+				File openFile = fileChooser.getSelectedFile();
 
+				// open file and set text
+				try (BufferedReader br = new BufferedReader(new FileReader(openFile))) {
+					StringBuilder sb = new StringBuilder();
+					String line;
+					while ((line = br.readLine()) != null) {
+						sb.append(line);
+						sb.append("\n");
+					}
+
+					String text = sb.toString();
+					MainFrame.this.inputArea.setText(text);
+					MainFrame.this.sequencePanel.drawSequence(text);
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
 			}
 		}
 
